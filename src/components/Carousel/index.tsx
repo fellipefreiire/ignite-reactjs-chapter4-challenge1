59793a3dcbbd6692315b096cb36502e842389a73
Felipe <fellipefreiire3@gmail.com>
2021-11-30 02:18:52 -0300

    homepage mobile ready

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -11,7 +11,7 @@ export const Carousel: React.FC = (): JSX.Element => {
   const router = useRouter()
 
   return (
-    <Box w='1240px' h='450px' m='auto' mb='40px'>
+    <Box w={['375px', '1240px']} h={['250px', '450px']} m='auto' mb={['24px', '40px']}>
       <Swiper
         navigation
         pagination={{
@@ -26,15 +26,15 @@ export const Carousel: React.FC = (): JSX.Element => {
               justifyContent='center'
               alignItems='center'
               flexDirection='column'
-              h='450px'
+              h={['250px', '450px']}
               bgImage={`url('${continent.bgImageUrl}')`}
               backgroundSize='cover'
               backgroundPosition='left'
               onClick={() => router.push(`${continent.pageUrl}`)}
             >
               <Box textAlign='center'>
-                <Text fontSize='48px' fontWeight='700' color='light.text'>{continent.name}</Text>
-                <Text fontSize='24px' fontWeight='700' color='light.info'>{continent.description}</Text>
+                <Text fontSize={['24px', '48px']} fontWeight='700' color='light.text'>{continent.name}</Text>
+                <Text fontSize={['14px', '24px']} fontWeight='700' color='light.info'>{continent.description}</Text>
               </Box>
             </Flex>
           </SwiperSlide>
--- src/components/TravelTypes/index.tsx
+++ src/components/TravelTypes/index.tsx
@@ -1,30 +1,49 @@
-import { Flex, Text } from "@chakra-ui/react"
+import { Box, Flex, Text } from "@chakra-ui/react"
 import Image from 'next/image'
 import { data } from './content'
 
 export const TravelTypes: React.FC = (): JSX.Element => {
   return (
     <Flex
-      w='1160px'
+      w={['375px', '1160px']}
       h='145'
-      justifyContent='space-between'
+      justifyContent={['space-between', 'space-between']}
+      alignItems={['center', 'center']}
       m='auto'
-      mt='114px'
+      mt={['36px', '114px']}
+      pl={['50px', '0']}
+      pr={['50px', '0']}
+      wrap='wrap'
     >
       {data.map(item => (
         <Flex
           key={item.alt}
-          direction='column'
+          direction={['row', 'column']}
           justifyContent='center'
           alignItems='center'
+          m='auto'
         >
-          {/* <Image
-            src={item.img}
-            width={85}
-            height={85}
-            alt={item.alt}
-          /> */}
-          <Text fontSize='24px' fontWeight='600' mt='24px'>
+          <Box display={['none', 'block']}>
+            <Image
+              src={item.img}
+              width={85}
+              height={85}
+              alt={item.alt}
+            />
+          </Box>
+          <Box
+            display={['block', 'none']}
+            w='8px'
+            h='8px'
+            bgColor='highlight'
+            borderRadius='50%'
+          />
+          <Text
+            fontSize={['18px', '24px']}
+            fontWeight={['500', '600']}
+            mt={['0', '24px']}
+            ml={['8px', '0']}
+          >
             {item.text}
           </Text>
         </Flex>
